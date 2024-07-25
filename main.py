3a462b609ac620f5f731a39131895e583acbe35b
felipet27 <felipet27@gmail.com>
2024-07-24 20:57:23 -0500

    texto a mostrar

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {
+        "Mi nombre es" : "Andres Felipe",
+        "Próximo Ingeniero DevOps Junior de " : "Smart Talent",
+        "Y esta es la solucion de la prueba": "Nos vemos pronto"
+    }
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
